583f9dd7a3cfa6b4dbe6aa34f8323635fb3ecb2e
JACKSUMMERS2 <26564873-JACKSUMMERS2@users.noreply.replit.com>
2023-10-10 17:58:26 +0000

    Update Files

--- src/pages/AnnBiblio.tsx
+++ src/pages/AnnBiblio.tsx
@@ -1,3 +1,22 @@
-import * as util from './util'
+import * as util from './util';
+import './App.css';
 
-export default function 
+
+export default function AnnBiblio(){
+  return(
+    <Image name='/abbibliohdr.jpeg'>
+      <h1 id='anbiblohdrtxt'>Annotated Bibliography -- Mongol Empire</h1>
+    </Image>
+  )
+} 
+
+export const BibOne = () => {
+  return(
+    <util.Collapsable name='Source #1'>
+      <div className='ctncld'>
+        <h4></h4>
+      </div>
+    </util.Collapsable>
+  )
+}
+
