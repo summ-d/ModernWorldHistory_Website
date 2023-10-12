a5cc59ed320876eb041ba589519db8b7b8edb75c
summ-d <65038991+summ-d@users.noreply.github.com>
2023-10-11 21:22:11 -0700

    UpdateFiles

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import CommAfAs, { Awnsers } from './pages/CommAfAs';
 import StRleg, { Ans } from './pages/StRleg';
+import AnnBiblio, { WholeSec } from './pages/AnnBiblio';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -32,6 +33,18 @@ ReactDOM.createRoot(document.getElementById('strelgtop') as HTMLElement).render(
 ReactDOM.createRoot(document.getElementById('ans2') as HTMLElement).render(
   <React.StrictMode>
     <Ans />
+  </React.StrictMode>
+)
+
+ReactDOM.createRoot(document.getElementById('annbibtop') as HTMLElement).render(
+  <React.StrictMode>
+    <AnnBiblio />
+  </React.StrictMode>
+)
+
+ReactDOM.createRoot(document.getElementById('bib') as HTMLElement).render(
+  <React.StrictMode>
+    <WholeSec />
   </React.StrictMode>
 )
 
