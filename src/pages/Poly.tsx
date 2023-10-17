e693174d5d158031fdea60f9ade50e475611faaf
summ-d <65038991+summ-d@users.noreply.github.com>
2023-10-16 21:41:25 -0700

    Update files

---
+++ src/pages/Poly.tsx
@@ -0,0 +1,16 @@
+import * as util from './util';
+
+
+export default function OceanBanner(){
+    return(
+        <util.Image name='/oceania.jpeg'>
+            <h1 id='oceantop'>
+                Oceania -- Circa 1200CE-1450CE
+            </h1>
+        </util.Image>
+    )
+}
+
+export function SecBanOne(){
+    
+}
